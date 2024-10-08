47aea13c3474f6a4b6bf7493da6d1709542b287c
Moghammed <moghammed@gmail.com>
2024-10-08 14:19:10 +0200

    fix the buildpack

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from "astro/config";
 import { sharp } from "astro/assets/services/sharp";
+import node from "@astrojs/node";
 
 export default defineConfig({
   image: {
     service: sharp(),
   },
+  output: "server",
+  adapter: node({
+    mode: "standalone",
+  }),
 });
